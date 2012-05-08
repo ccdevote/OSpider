df3fa0545eb9a928e3ea9865b54feb794a698806
Horizon <ccdevote@gmail.com>
2012-05-08 23:17:52 +0800

    增加Fronter基类，并未做具体实现

---
+++ trunks/Horizon/src/com/horizon/spider/fronter/Fronter.java
@@ -0,0 +1,19 @@
+package com.horizon.spider.fronter;
+
+/**
+ * 链接制造工厂
+ * 描述：为Fetcher-抓取模块提供待抓取链接
+ * 		操作LinkQueue-链接队列：
+ * 			预处理：从LinkQueue取一定数目的链接
+ * 			分级  ：对预处理取得的链接按优先级分类，
+ * 				.将优先级高的放入优先队列
+ * 				.其次的放入普通队列
+ * 				.优先级最低的放入待定队列，以根据实际时间限制以及深度限制决定是否进行抓取
+ * 	        
+ * @author MZY
+ * 
+ *
+ */
+public abstract class Fronter {
+
+}
