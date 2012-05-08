d319992aeef3e357dce4f28479f8bea938154386
Horizon <ccdevote@gmail.com>
2012-05-08 23:58:20 +0800

    增加Proccesser类--处理器的主调类

---
+++ trunks/Horizon/src/com/horizon/spider/proccesser/Proccessor.java
@@ -0,0 +1,15 @@
+package com.horizon.spider.proccesser;
+
+/**
+ * 这个类是处理其主调类，用来调用定义的所有处理模块，用来对Fetcher模块抓取下来的内容进行处理
+ * 例如：网页去重
+ * 		URL评分
+ * 		相似度计算
+ * 		抽取链接
+ * @author Mzy
+ * @date 2012-05-08 23:42:00
+ * @version 1.0
+ */
+public class Proccessor {
+
+}
