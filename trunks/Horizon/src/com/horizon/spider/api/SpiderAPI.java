9efaa9b9b7ced9d4a75a7a0a20a7ff54f9ce8807
Horizon <ccdevote@gmail.com>
2012-05-10 00:04:06 +0800

    为SpiderAPI类增加了一些常用方法

--- trunks/Horizon/src/com/horizon/spider/api/SpiderAPI.java
+++ trunks/Horizon/src/com/horizon/spider/api/SpiderAPI.java
@@ -1,5 +1,22 @@
 package com.horizon.spider.api;
 
-public class SpiderAPI {
+import com.horizon.spider.spider.Spider;
+import com.horizon.spider.spider.Tasker;
 
+public class SpiderAPI {
+	private Spider spider = null;
+
+	public void initSpider(Tasker task) {
+		spider = new Spider(task);
+	}
+
+	public void startSpider() {
+		if (spider != null) {
+			spider.start();
+		}
+	}
+	
+	public void shutDownSpider(){
+		
+	}
 }
